87e9f9aa02922ae3b13e51823c5f369cae52b057
Eric Stone <home@erics-air.lan>
2020-08-24 21:59:39 -0400

    solution with one sort of modified binary search

--- introduction_and_environment/hello_world/2_python_caches/solution/solution.py
+++ introduction_and_environment/hello_world/2_python_caches/solution/solution.py
@@ -1,26 +1,41 @@
 LOWER_BOUND = 0
-UPPER_BOUND = 0
+# UPPER_BOUND = 0
 
-x = 1
-y = 1
+###Number needs to be positive
 
-while True:
-	# testcase = 1234
-
+def upper_binary_search(testcase):
+	x = testcase+1
+	y = testcase+1
 	if id(x) != id(y):
-		UPPER_BOUND = x-1
-		break
+		x//=2
+		y//=2
+		# print(y)
+		if id(x) == id(y):
+			while id(x) == id(y):
+				x+=1
+				y+=1
+				# print(y)
+			# UPPER_BOUND = x
+			# print(UPPER_BOUND)
+			return(x-1)
+		else:
+			return(upper_binary_search(x))
+			# return()
 	else:
-		x += 1
-		y += 1
+		return("Int is too low")
 
-x = 1
-y = 1
-
-while True:
-	if id(x) != id(y):
-		LOWER_BOUND = x+1
-		break
+def lower_search(testcase):
+	x = testcase+1
+	y = testcase+1
+	if id(x) == id(y):
+		while id(x) == id(y):
+			x-=1
+			y-=1
+		return(x+1)
 	else:
-		x -= 1
-		y -= 1
+		return("Int is too high")
+
+UPPER_BOUND = upper_binary_search(1233)
+LOWER_BOUND = lower_search(0)
+# print(UPPER_BOUND)
+# print(LOWER_BOUND)
