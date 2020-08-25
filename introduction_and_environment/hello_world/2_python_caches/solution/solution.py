a1737071c8b2778f4da11f7150ecf20346230053
Eric Stone <home@erics-air.lan>
2020-08-24 20:59:24 -0400

    finished 2_python_caches

--- introduction_and_environment/hello_world/2_python_caches/solution/solution.py
+++ introduction_and_environment/hello_world/2_python_caches/solution/solution.py
@@ -1,23 +1,26 @@
 LOWER_BOUND = 0
 UPPER_BOUND = 0
 
+x = 1
+y = 1
+
 while True:
 	# testcase = 1234
-	x = 1234
-	y = 1234
-	if id(x) == id(y):
-		UPPER_BOUND = ++x
+
+	if id(x) != id(y):
+		UPPER_BOUND = x-1
+		break
+	else:
+		x += 1
+		y += 1
+
+x = 1
+y = 1
+
+while True:
+	if id(x) != id(y):
+		LOWER_BOUND = x+1
 		break
 	else:
 		x -= 1
 		y -= 1
-		print (x)
-
-while True:
-	x = 1
-	y = 1
-	if id(x) != id(y):
-		LOWER_BOUND = ++x
-		break
-	else:
-		x = y = --x
