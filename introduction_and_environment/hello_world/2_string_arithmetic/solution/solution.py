e4b882f2642eeab52f8c036dbcedd7d152016508
Eric Stone <home@erics-air.lan>
2020-08-26 16:24:35 -0400

    finished hello_world/2_string_arithmetic

--- introduction_and_environment/hello_world/2_string_arithmetic/solution/solution.py
+++ introduction_and_environment/hello_world/2_string_arithmetic/solution/solution.py
@@ -1,1 +1,89 @@
 # Code your solution here
+
+STR_ARITHMETIC = 0
+string1 = "Pokemon!"
+string2 = " Gotta Catch 'em All!"
+
+try:
+    string1 + 1
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+
+try:
+    string1 - 1
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+
+try:
+    string1 * 1
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+
+try:
+    string1 / 1
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+
+try:
+    string1 ** 1
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+
+try:
+    string1 // 1
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+
+try:
+    string1 % 1
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+
+try:
+    string1 + string2
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+
+try:
+    string1 - string2
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+
+try:
+    string1 * string2
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+
+try:
+    string1 / string2
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+
+try:
+    string1 ** string2
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+
+try:
+    string1 // string2
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+
+try:
+    string1 % string2
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
