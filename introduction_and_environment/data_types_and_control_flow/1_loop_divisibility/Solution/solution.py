c8ec2f45762c6d80b6e6e6a08838f6d010f0834d
Eric Stone <stone.ericm@gmail.com>
2020-08-28 15:51:47 -0400

    finished data_types_and_control_flow/1_loop_divisibility

--- introduction_and_environment/data_types_and_control_flow/1_loop_divisibility/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/1_loop_divisibility/Solution/solution.py
@@ -1,2 +1,20 @@
 # Code your solution here
-       
+import random
+
+x = random.randrange(0, 50)
+
+data = []
+
+for each in range(0, x, 5):
+    if each == 0:
+        pass
+    else:
+        data.append(each)
+for each in range(0, x, 7):
+    if each == 0:
+        pass
+    else:
+        data.append(each)
+
+
+print(data)
