93e0e82f5ec1c1f00022aebddc16da543f979168
Eric Stone <stone.ericm@gmail.com>
2020-08-28 16:03:08 -0400

    finished data_types_and_control_flow/1_string_convert (also why don't the tests allow for input args?)

--- introduction_and_environment/data_types_and_control_flow/1_string_convert/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/1_string_convert/Solution/solution.py
@@ -1,1 +1,11 @@
 # Code your solution here
+string = input()
+
+if string.isupper():
+    string = string.lower()
+    print(string)
+elif string.islower():
+    string = string.upper()
+    print(string)
+else:
+    print("Input error")
